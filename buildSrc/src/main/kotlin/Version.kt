bdcdc69747f8163e00fb37232e89972c42dee131
Michal Kocian <michal@kocian.name>
2021-01-23 13:53:27 +0000

    Add okhttp and mock web server dependencies

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -3,6 +3,7 @@ object Version {
 
     object Dependency {
         const val kotlinSerialisation = "1.0.1"
+        const val okhttp = "4.9.0"
     }
 
     object Test {
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -13,7 +13,9 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Version.Dependency.kotlinSerialisation}")
+    implementation("com.squareup.okhttp3:okhttp:${Version.Dependency.okhttp}")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.Test.junit5}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.Test.junit5}")
+    testImplementation("com.squareup.okhttp3:mockwebserver:${Version.Dependency.okhttp}")
 }
