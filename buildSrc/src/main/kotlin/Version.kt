a8fc93e3e607f67c13e9e1f80d4aec79098e3bef
Michal Kocian <michal@kocian.name>
2021-01-23 13:27:10 +0000

    Export JUnit5 version

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,3 +1,7 @@
 object Version {
     const val kotlin = "1.4.21"
+
+    object Test {
+        const val junit5 = "5.7.0"
+    }
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -12,6 +12,6 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.Test.junit5}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.Test.junit5}")
 }
