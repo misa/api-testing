17c30c0ce320f08d7b34f5dafa36877d266bb7f3
Michal Kocian <michal@kocian.name>
2021-01-23 13:27:48 +0000

    Add Kotlin Serialisation library

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,6 +1,10 @@
 object Version {
     const val kotlin = "1.4.21"
 
+    object Dependency {
+        const val kotlinSerialisation = "1.0.1"
+    }
+
     object Test {
         const val junit5 = "5.7.0"
     }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version Version.kotlin
+    kotlin("plugin.serialization") version Version.kotlin
 }
 
 description = "API testing helpers"
@@ -11,6 +12,7 @@ tasks.withType<Test> {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Version.Dependency.kotlinSerialisation}")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.Test.junit5}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.Test.junit5}")
