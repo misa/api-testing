3c8df4923ae1bc4cb2216a15a6fb803650ee1606
Michal Kocian <michal@kocian.name>
2021-02-06 19:45:40 +0000

    Logging interceptor dependency

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Version.Dependency.kotlinSerialisation}")
     implementation("com.squareup.okhttp3:okhttp:${Version.Dependency.okhttp}")
+    implementation("com.squareup.okhttp3:logging-interceptor:${Version.Dependency.okhttp}")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.Test.junit5}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.Test.junit5}")
