885f3d9772f43cd04510f7e5509a26b62ca1f366
Michal Kocian <michal@kocian.name>
2021-02-06 19:43:28 +0000

    Target Java 1.8

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -9,6 +9,12 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
