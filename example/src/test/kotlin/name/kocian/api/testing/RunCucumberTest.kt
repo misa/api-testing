046c10c55cbed5bc7b1f8f69c25875766f6b34d9
Michal Kocian <michal@kocian.name>
2021-02-07 22:24:40 +0000

    Glue and feature folders configuration

--- example/src/test/kotlin/name/kocian/api/testing/RunCucumberTest.kt
+++ example/src/test/kotlin/name/kocian/api/testing/RunCucumberTest.kt
@@ -1,7 +1,12 @@
 package name.kocian.api.testing
 
 import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
+@CucumberOptions(
+    glue = ["name.kocian.api.testing"],
+    features = ["src/test/resources/features"]
+)
 @RunWith(Cucumber::class)
 class RunCucumberTest
